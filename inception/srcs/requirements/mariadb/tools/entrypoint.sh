ecea17e1c8386445af814a3d53638cdae675c1e7
Ricardo <rickyamas@gmail.com>
2024-01-18 03:00:44 -0300

    sera que e necessario my.cnf?

--- inception/srcs/requirements/mariadb/tools/entrypoint.sh
+++ inception/srcs/requirements/mariadb/tools/entrypoint.sh
@@ -1,29 +1,37 @@
 #!/bin/bash
 
+# Iniciando o mysqld_safe em segundo plano
 mysqld_safe --skip-syslog &
 
+# Aguardando até que o servidor MariaDB esteja pronto para aceitar conexões
 while ! mysqladmin ping -hlocalhost --silent; do
     sleep 1
 done
 
-if ! mysql -e "USE $WP_DATABASE;";
-
-then
-
-    mysql -e "CREATE DATABASE $WP_DATABASE;"
-    mysql -e "CREATE USER '$WP_ADMIN_USER'@'%' IDENTIFIED BY '$WP_ADMIN_PWD';"
-    mysql -e "GRANT ALL PRIVILEGES ON $WP_DATABASE.* TO '$WP_ADMIN_USER'@'%';"
+# Verificando se o banco de dados já existe
+if ! mysql -e "USE $MYSQL_DATABASE;"; then
+    # Criando o banco de dados, usuário e concedendo privilégios
+    mysql -e "CREATE DATABASE $MYSQL_DATABASE;"
+    mysql -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_USER_PASSWORD';"
+    mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_USER_PASSWORD';"
     mysql -e "FLUSH PRIVILEGES;"
 
     echo "Database created."
 else
-    echo "Database '$WP_DATABASE' has already been created."
+    echo "Database '$MYSQL_DATABASE' has already been created."
 fi
 
+# Alterando permissões dentro do script
+chmod 755 /var/www/html
+chown -R www-data:www-data /var/www/html
+
+# Desligando o servidor MariaDB
 mysqladmin shutdown
 
+# Aguardando até que o servidor seja completamente encerrado
 while mysqladmin ping -hlocalhost --silent; do
     sleep 1
 done
 
+# Executando o servidor MariaDB novamente
 exec mariadbd
